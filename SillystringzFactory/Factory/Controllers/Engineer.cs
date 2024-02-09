3c12a0190f10aa2f8daf087a9840248a8e240f4a
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-02-09 13:34:08 -0800

    Update functionality to add Machines to an Engineer

--- SillystringzFactory/Factory/Controllers/Engineer.cs
+++ SillystringzFactory/Factory/Controllers/Engineer.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
 using Factory.Models;
@@ -30,6 +32,38 @@ namespace Factory.Controllers
     public ActionResult Create()
     {
       return View();
+    }
+
+    public ActionResult Detail(int id)
+    {
+      Engineer selectedEngineer = _db.Engineers
+        .Include(e => e.JoinEntities)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(e => e.EngineerId == id);
+      return View(selectedEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int machineId)
+    {
+      #nullable enable
+      EngineerMachine? joinEntity = _db.EngineerMachines.FirstOrDefault(j => j.MachineId == machineId && j.EngineerId == engineer.EngineerId);
+      #nullable disable
+
+      if (joinEntity == null && machineId != 0)
+      {
+        _db.EngineerMachines.Add(new EngineerMachine() { MachineId = machineId, EngineerId = engineer.EngineerId });
+        _db.SaveChanges();
+      }
+
+      return RedirectToAction("Detail", new { id = engineer.EngineerId });
+    }
+
+    public ActionResult AddMachine(int id)
+    {
+      Engineer selectedEngineer = _db.Engineers.FirstOrDefault(e => e.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View(selectedEngineer);
     }
   }
 }
