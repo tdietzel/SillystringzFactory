4d2f58cb2e44439a6837182df04d153647e4bda3
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-02-09 10:39:18 -0800

    Add Create & Read functionality for Machine class

---
+++ SillystringzFactory/Factory/Controllers/Machine.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class MachineController : Controller
+  {
+    private readonly FactoryContext _db;
+    public MachineController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List <Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
